f2b57fc6650fee73d26863fbe4e400a2bbf5dfa8
AntonGorynyae <antongorynya1@gmail.com>
2024-02-19 18:29:36 +0300

    add time

--- server.py
+++ server.py
@@ -2,8 +2,26 @@ from aiohttp import web
 import aiofiles
 
 
+INTERVAL_SECS = 1
+
+
 async def archive(request):
-    raise NotImplementedError
+    archive_hash = request.match_info.get('archive_hash')
+    print(f"Archive Hash: {archive_hash}")
+
+    response = web.StreamResponse()
+
+    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
+    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
+    response.headers['Content-Type'] = 'text/html'
+    # Отправляет клиенту HTTP заголовки
+    await response.prepare(request)
+    while True:
+        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
+        # Отправляет клиенту очередную порцию ответа
+        await response.write(message.encode('utf-8'))
+        await asyncio.sleep(INTERVAL_SECS)
 
 
 async def handle_index_page(request):

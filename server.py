44e2d22230bf75d44977c4732c386fcb74c83fae
Anton Gorynya <antongorynya1@gmail.com>
2024-02-20 17:47:12 +0300

    async download

--- server.py
+++ server.py
@@ -1,28 +1,37 @@
-from aiohttp import web
+from aiohttp import web, ClientSession
 import aiofiles
+import asyncio
 
 
 INTERVAL_SECS = 1
 
 
 async def archive(request):
+    chunk_size = 100
     archive_hash = request.match_info.get('archive_hash')
-    print(f"Archive Hash: {archive_hash}")
+    command = ['zip','-jr', '-', f'test_photos/{archive_hash}/']
+
+    proc = await asyncio.create_subprocess_exec(
+        *command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE)
+
 
     response = web.StreamResponse()
-
-    # Большинство браузеров не отрисовывают частично загруженный контент, только если это не HTML.
-    # Поэтому отправляем клиенту именно HTML, указываем это в Content-Type.
     response.headers['Content-Type'] = 'text/html'
+    response.headers['Content-Disposition'] = 'attachment; filename="archive.zip"'
     # Отправляет клиенту HTTP заголовки
     await response.prepare(request)
-    while True:
-        formatted_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        message = f'{formatted_date}<br>'  # <br> — HTML тег переноса строки
-        # Отправляет клиенту очередную порцию ответа
-        await response.write(message.encode('utf-8'))
-        await asyncio.sleep(INTERVAL_SECS)
-
+    
+    
+    async with ClientSession() as session:        
+        while True:
+            part = await proc.stdout.read(chunk_size)
+            if not part:
+                break                
+            await response.write(part)
+    return response
+   
 
 async def handle_index_page(request):
     async with aiofiles.open('index.html', mode='r') as index_file:
